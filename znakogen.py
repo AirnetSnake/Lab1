c58929f3a5012502161a834fb5af497c61b7235b
Ivan <airnetsnake@gmail.com>
2012-09-25 19:08:35 +0300

    +znakogen fixed

--- znakogen.py
+++ znakogen.py
@@ -1,4 +1,6 @@
-#Сделал только до 3, т.к. или я неправильно понял смысл задачи
+# -*- encoding: utf-8 -*-
+#
+#Сделал только до 4,т.к. смысл и так понятен
 def zgen():
 	n = raw_input('Enter number 1-4: ')
 	if n is not int:
@@ -6,7 +8,7 @@ def zgen():
 		return false
 	nums={1:'    *\n    *\n    *\n    *\n    *\n    *\n   ***\n',
 		2:' \n  ***\n     *\n     *\n    *\n   *\n  ****\n ',
-		3:'\n  ****\n     *\n    *\n     *\n  *  *\n   **\n ')
+		3:'\n  ****\n     *\n    *\n     *\n  *  *\n   **\n ',
 		4:'     *\n    **\n   * *\n  *  *\n  ****\n     *\n     *\n     *')
 	print nums[n]
 	
